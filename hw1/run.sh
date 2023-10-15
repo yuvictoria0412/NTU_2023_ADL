201634328b5dfa2be8ccfe44282ea014b7379975
yuvictoia0412 <yuvictoria0412@gmail.com>
2023-10-16 00:16:21 +0800

    tidy up

--- hw1/run.sh
+++
@@ -1,8 +0,0 @@
-pip install -U -r "requirements.txt"
-# pip install -U git+https://github.com/huggingface/accelerate
-
-accelerate launch run_swag_no_trainer.py \
-  --model_name_or_path bert-base-uncased \
-  --dataset_name swag \
-  --output_dir /tmp/test-swag-no-trainer \
-  --pad_to_max_length
